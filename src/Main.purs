440043daca5d5c18f461e84d948f08e3e189961d
Barrytech <barryabdoul2024@gmail.com>
2018-07-16 16:24:34 -0400

    Project init. Fixes #3

--- src/Main.purs
+++ src/Main.purs
@@ -3,15 +3,52 @@ module Main where
 import Prelude
 
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import DOM (DOM)
+import DOM.HTML (window) as DOM
+import DOM.HTML.Types (htmlDocumentToParentNode) as DOM
+import DOM.HTML.Window (document) as DOM
+import DOM.Node.ParentNode (querySelector, QuerySelector(..)) as DOM
+import Data.Maybe (Maybe, fromJust)
+import Partial.Unsafe (unsafePartial)
+import React (ReactComponent)
+import React (createFactory) as R
+import React.DOM (text, button, p') as R
+import React.DOM.Props as RP
+import ReactDOM as RDOM
+import Thermite as T
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Welcome to New lab!"
-  log $ showAge 17
+displayState :: Int -> String
+displayState n = "The state is: " <> (show n)
 
-showAge :: Int -> String
-showAge age = "My age is " <> (show age)
+data Action = Increment | Decrement
 
-showTwoThings :: Int -> Int -> String
-showTwoThings a b = "The two things are: " <> show a <> " and " <> show b
+type State = { counter :: Int }
+
+initialState :: State
+initialState = { counter: 0 }
+
+render :: forall a. T.Render State a Action
+render dispatch _ state _ =
+  [ R.p' [ R.text "Hello NewLab!\n"
+         , R.text $ displayState state.counter
+         ]
+  , R.p' [ R.button [ RP.onClick \_ -> dispatch Increment ]
+                    [ R.text "Increment" ]
+         , R.button [ RP.onClick \_ -> dispatch Decrement ]
+                    [ R.text "Decrement" ]
+         ]
+  ]
+
+performAction :: forall a b. T.PerformAction a State b Action
+performAction Increment _ _ = void $ T.cotransform $ \state -> state { counter = state.counter + 10 }
+performAction Decrement _ _ = void $ T.cotransform $ \state -> state { counter = state.counter - 1 }
+
+spec :: forall a b. T.Spec a State b Action
+spec = T.simpleSpec performAction render
+
+main :: forall eff. Eff (dom :: DOM | eff) (Maybe ReactComponent)
+main = unsafePartial do
+  let component = T.createClass spec initialState
+  document <- DOM.window >>= DOM.document
+  container <- fromJust <$> DOM.querySelector (DOM.QuerySelector "#container") (DOM.htmlDocumentToParentNode document)
+  RDOM.render (R.createFactory component {}) container
