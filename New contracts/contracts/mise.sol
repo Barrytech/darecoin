5977bea9f43b6d6c66b0ac0395b5068b387ad97e
Barrytech <barryabdoul2024@gmail.com>
2018-07-31 15:31:05 -0400

    updated

---
+++ New contracts/contracts/mise.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.4.24;
+
+contract PriceGamble{
+
+  address owner;
+  uint round;
+  uint constant price = 1 ether;
+mapping (address => uint) public players;
+
+
+ //constructor
+ function Constructor(){
+owner = msg.sender;
+
+
+
+
+ }
+
+ /* fallback function
+ allow users to money from wallet to the address directly
+ */
+
+function () payable{
+BetEther(1);
+
+
+
+
+}
+
+
+
+function BetEther (uint amount) payable {
+
+
+  if (msg.value != price  || players != 2){
+    throw;
+  }
+  players[msg.sender] += players;
+  players -= players;
+
+  if (players  == 0){
+    selfdestruct(owner);
+  }
+
+}
+}
