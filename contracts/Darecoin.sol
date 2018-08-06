52d3b4f5d9b7f80e46daacf1129a4d2d5e23ef1b
kejace <kejace@gmail.com>
2018-08-06 13:05:37 -0400

    Probably works

--- contracts/Darecoin.sol
+++ contracts/Darecoin.sol
@@ -2,11 +2,19 @@ pragma solidity ^0.4.23;
 
 import "./Game.sol";
 
+
+contract MakerDAO {
+  function peek() external view returns(bytes32, bool);
+  function read() external view returns(bytes32);
+}
+
 contract Darecoin is Game {
 
   enum gamePlay {DOWN, UP} // DOWN = 0, UP = 1
 
-  address makerDAO = 0x0;
+  bytes32 private startPrice;
+  uint startBlock;
+  MakerDAO makerDAO = MakerDAO(0xE39451e34f8FB108a8F6d4cA6C68dd38f37d26E3); // rinkeby
 
   constructor(
         address _feeAddress,
@@ -14,61 +22,49 @@ contract Darecoin is Game {
         uint256 _stakeSize,
         uint256 _maxp,
         uint256 _gameStageLength,
-        address _nptAddress,
-        address _makerDAOAddress // <-- the address to our makerDAO contract
+        address _nptAddress // 0x805cdcf68cc12220f89425be89daa44dbc83ff30
         )
-          public Game(_feeAddress, _gameFeePercent, _stakeSize, _maxp, _gameStageLength, _nptAddress) {
-    makerDAO = _makerDAOAddress;
+        public Game(_feeAddress, _gameFeePercent, _stakeSize, _maxp, _gameStageLength, _nptAddress) {
+  }
+
+  function commit(bytes32 hashedCommit) public payable whenNotPaused {
+      
+    // if the first player is playing
+    if (state.currNumberCommits == 0) {
+        startPrice = makerDAO.read();
+        startBlock = block.number;
+    }
+    super.commit(hashedCommit);
   }
 
   function checkGuess(string guess) private {
     uint guessAsInt = stringToUint(guess);
-    require(guessAsInt == 0 || guessAsInt == 1);
+    require(guessAsInt == uint(gamePlay.DOWN) || guessAsInt == uint(gamePlay.UP));
   }
 
   function findWinners() private {
 
     address[] memory winners = new address[](config.MAX_PLAYERS);
 
-    uint rightResult = 1; // assume UP is the right answer
+    bytes32 newPrice = makerDAO.read();
 
-    for(uint256 i = 0; i < state.currNumberReveals; i++){
-      uint numWinners = 0;
-      uint256 playForPlayer = stringToUint(gameData[gameDataKeys[i]]);
-
-      if(playForPlayer == rightResult){
-        numWinners++;
-        winners[numWinners] = gameDataKeys[i];
-      }
+    if(block.number > startBlock + 240) // wait approximately one hour
+    
+        uint rightResult = newPrice > startPrice ? uint(gamePlay.UP) : uint(gamePlay.DOWN);
+    
+        for(uint256 i = 0; i < state.currNumberReveals; i++){
+          uint numWinners = 0;
+          uint256 playForPlayer = stringToUint(gameData[gameDataKeys[i]]);
+    
+          if(playForPlayer == rightResult){
+            winners[numWinners] = gameDataKeys[i];
+            numWinners++;
+          }
+        }
+    
+        uint256 prize = address(this).balance.div(numWinners);
+        performPayout(winners, numWinners, prize);
+    
     }
-
-    uint256 prize = address(this).balance.div(numWinners);
-    performPayout(winners, numWinners, prize);
-
-  }
-}
-
-//here start the contract to mise your ether
-contract EtherBet{
-  address player;
-  uint NumberofPlayers;
-  uint constant betval = 10000000000000;
-
-  mapping (address => uint) public players;
-  
-  function BetEther () public {
-    player = msg.sender;
-    NumberofPlayers = 2;
-  }
-
-  function Paybet(uint amount) public payable {
-    require(msg.value == betval);
-
-    NumberofPlayers = amount;
-
-    if (NumberofPlayers != 2 ){
-      selfdestruct(player);
-    }
-  }
 }
 
