d6c9b4e98b57d66fde0dd00db8d8e9a5ff8df4d5
Barrytech <barryabdoul2024@gmail.com>
2018-08-06 13:29:33 -0400

    changed input form

--- contracts/Darecoin.sol
+++ contracts/Darecoin.sol
@@ -1,4 +1,164 @@
-pragma solidity ^0.4.24;
+
+pragma solidity ^0.4.23;
+
+import "./Game.sol";
+
+
+contract MakerDAO {
+  function peek() external view returns(bytes32, bool);
+  function read() external view returns(bytes32);
+}
+
+contract Darecoin is Game {
+
+  enum gamePlay {DOWN, UP} // DOWN = 0, UP = 1
+
+  bytes32 private startPrice;
+  uint startBlock;
+  MakerDAO makerDAO = MakerDAO(0xE39451e34f8FB108a8F6d4cA6C68dd38f37d26E3); // rinkeby
+
+  constructor(
+        address _feeAddress,
+        uint256 _gameFeePercent,
+        uint256 _stakeSize,
+        uint256 _maxp,
+        uint256 _gameStageLength,
+        address _nptAddress // 0x805cdcf68cc12220f89425be89daa44dbc83ff30
+        )
+        public Game(_feeAddress, _gameFeePercent, _stakeSize, _maxp, _gameStageLength, _nptAddress) {
+  }
+
+  function commit(bytes32 hashedCommit) public payable whenNotPaused {
+
+    // if the first player is playing
+    if (state.currNumberCommits == 0) {
+        startPrice = makerDAO.read();
+        startBlock = block.number;
+    }
+    super.commit(hashedCommit);
+  }
+
+  function checkGuess(string guess) private {
+    uint guessAsInt = stringToUint(guess);
+    require(guessAsInt == uint(gamePlay.DOWN) || guessAsInt == uint(gamePlay.UP));
+  }
+
+  function findWinners() private {
+
+    address[] memory winners = new address[](config.MAX_PLAYERS);
+
+    bytes32 newPrice = makerDAO.read();
+
+    if(block.number > startBlock + 240) // wait approximately one hour
+
+        uint rightResult = newPrice > startPrice ? uint(gamePlay.UP) : uint(gamePlay.DOWN);
+
+        for(uint256 i = 0; i < state.currNumberReveals; i++){
+          uint numWinners = 0;
+          uint256 playForPlayer = stringToUint(gameData[gameDataKeys[i]]);
+
+          if(playForPlayer == rightResult){
+            winners[numWinners] = gameDataKeys[i];
+            numWinners++;
+          }
+        }
+
+        uint256 prize = address(this).balance.div(numWinners);
+        performPayout(winners, numWinners, prize);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* pragma solidity ^0.4.24;
 
 import "./Game.sol";
 
@@ -149,4 +309,4 @@ function OnUser2Wins(){
 
 
 
-//contract ends there
+//contract ends there */
