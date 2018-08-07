4a8d8387bb4b23dba3f7303c6914dd8039a13d81
kejace <kejace@gmail.com>
2018-08-07 12:50:15 -0400

    Added deployment script

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,12 @@
+var Darecoin = artifacts.require("./Darecoin.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy( Darecoin
+                 , 0x0c762C4a8D894a7107c7A77aC10029a09Bddb445 // Fees goes to Barry's metamask 
+                 , 0 // game-fee percent
+                 , 1 // stake size
+                 , 2 // max players
+                 , 1 // game-stage length
+                 , 0x805cdcf68cc12220f89425be89daa44dbc83ff30 // npt address
+                 );
+};
--- truffle.js
+++ truffle.js
@@ -1,18 +1,14 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic = "august net desert drastic hover avoid soldier film sweet actual income suggest";
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    rinkeby: {
+      provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/"),
+      network_id: 4,
+      gas: 4992222,
+      gasPrice: 22000000000 // Specified in Wei
+    }
+  }
 };
